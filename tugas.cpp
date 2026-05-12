925a99df834caa9d05de9b5aee49ec82aba2ad6e
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-12 17:03:48 +0700

    implement User class with id generation and member variables

--- tugas.cpp
+++ tugas.cpp
@@ -2,3 +2,24 @@
 #include <vector>
 using namespace std;
 
+class User {
+protected:
+    static int globalId;
+
+public:
+    int id;
+    string nama;
+    string email;
+
+    User(string pNama, string pEmail) {
+        nama = pNama;
+        email = pEmail;
+        id = generateId();
+    }
+
+    int generateId() {
+        globalId++;
+        return globalId;
+    }
+};
+
