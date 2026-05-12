c3a55d1517372eba11375a2a042685e6fe776cc8
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-12 16:51:40 +0700

    implementasi prosedur cetakDokter() dan tambahanPasien() untuk mengelola daftar dokter dan pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -55,3 +55,17 @@ void pasien::tambahanDokter(dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 };
 
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter dari Pasien \"" << this->nama << "\";\n";
+    for (auto& a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter::tambahanPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahanDokter(this);
+}
+
