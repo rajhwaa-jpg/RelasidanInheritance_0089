a45fcd20489863b3f36e5f13beb98100c34b4aad
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-12 16:44:38 +0700

    pembuatan constructor dan destructor dari class pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -13,4 +13,13 @@ public:
     string nama;
     vector<dokter*> daftar_dokter;
 
- 
+    //pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "pasien \"" << nama << "\"" << " tidak ada\n";
+    };
+
+    
